30d3a1303316902245ea1ebd6073f107fb257b18
Duncan <duncan@thejoinary.com>
2023-02-03 10:17:17 +1100

    Add application/json header

--- netlify/functions/client.js
+++ netlify/functions/client.js
@@ -66,6 +66,9 @@ exports.handler = async event => {
 
     return {
         statusCode: 200,
+        headers: {
+            "Content-Type": "application/json"
+        },
         body: JSON.stringify({
             response_type: 'ephemeral',
             blocks
